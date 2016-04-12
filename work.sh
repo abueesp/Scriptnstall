5110f817997cf274bd562fab5d72bae7d95f7a01
Alfredo Bueno <abueesp@gmail.com>
2016-04-12 20:55:48 +0200

    Create work.sh

---
+++ work.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+#Bluetooth
+sudo sed -i 's/InitiallyPowered = true/InitiallyPowered = false/g' /etc/bluetooth/main.conf
+rfkill block bluetooth
+#mirror
+sudo sed -i 's|http://us.archive.ubuntu.com/ubuntu|http://mirrors.mit.edu/ubuntu|g' /etc/apt/sources.list
+sudo cp bash.bashrc /etc/bash.bashrc
+#SSH
+sudo apt-get install ssh -y
+newsshkey
+sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/gnome-keyring-ssh.desktop
+sudo sed -i 's/PermitRootLogin without password/PermitRootLogin no/' /etc/ssh/sshd_config #noroot
+sudo sed -i 's/Port **/Port 1022/' /etc/ssh/sshd_config #SSH PORT OTHER THAN 22, SET 1022
+sudo /etc/init.d/ssh restart
+sudo chown -R $USER:$USER .ssh
+sudo chmod -R 600 .ssh
+sudo chmod +x .ssh
+#Docker 
+sudo wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker $USER
+sudo apt-get purge apparmor -y
+sudo rm -rf /etc/apparmor.d/
+sudo rm -rf /etc/apparmor
+
+#UFW
+sudo apt-get install gufw -y
+sudo ufw enable
+sudo ufw allow 1022/tcp
+sudo iptables -F
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 1022 -j ACCEPT
+sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT ACCEPT ##If you are a server change this to DROP OUTPUT connections by default too
+sudo iptables -P FORWARD DROP
+sudo iptables restart
+sudo service avahi-daemon stop
+sudo cupsctl -E --no-remote-any
+sudo service cups-browsed stop
+#Some tools
+sudo apt-get install traceroute
+sudo apt-get install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sudo sed -ir 's/ZSH_THEME="robbyrussell"/ZSH_THEME="norm"/g' ~/.oh-my-zsh #noroot
+sudo apt-get install iotop
+sudo apt-get install fish -y
+sudo apt-get install byobu -y
+sudo apt-get install autojump -y
+sudo apt-get install nmap -y
+sudo apt-get install tmux -y
+sudo apt-get install htop -y
+sudo apt-get install fail2ban -y
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+echo "Please write down an email to send you notifications when someone is attacking your ports: "
+read email
+echo "You entered: $email"
+sed "s/destemail = your_email@domain.com/destemail = $email/g" /etc/fail2ban/jail.local
+sed "s/action = %(action_)s/action = %(action_mw)s/g" /etc/fail2ban/jail.local
+sed -e "s/enabled  = false/enabled  = true/g" /etc/fail2ban/jail.local
+sudo apt-get install zenmap -y
+sudo apt-get install virtualbox -y
+sudo apt-get install gparted -y
+sudo apt-get install nemo -y
+sudo apt-get install apt-get install amarok -y
+sudo apt-get install thunderbird -y
+thunderbird https://addons.mozilla.org/thunderbird/downloads/latest/775/addon-775-latest.xpi
+sudo apt-get install firefox -y
+firefox https://addons.mozilla.org/firefox/downloads/file/271802/no_more_install_delay-3.0-fx+sm+fn+tb.xpi
+
+
+
+readonly version="24.5"
+sudo rm emacs-"$version".tar.xz
+sudo rm -r emacs-"$version"
+#spacemacs
+sudo git clone http://github.com/syl20bnr/spacemacs ~/.emacs.d
+sudo mv $(sudo find / -name .emacs.d) .emacs.d.bak
+sudo mv $(sudo find -name .emacs) .emacs.bak 
+##plugins
+cd ~/.emacs.d
+wget http://github.com/ethereum/emacs-solidity/blob/master/solidity-mode.el ##solidity
+(add-to-list load-path "~/.emacs.d/") 
+(load "myplugin.el")
+cd
+sudo rm emacs-"$version".tar.xz
+
+
+##Github
+sudo apt-get install git -y 
+git config --global credential.helper cache
+# Set git to use the credential memory cache
+git config --global credential.helper 'cache --timeout=3600'
+# Set the cache to timeout after 1 hour (setting is in seconds)
+while true; do
+    read -p "Please set your username: " myusername $1
+    git config --global user.name $1
+    break
+done
+while true; do
+    read -p "Please set your email: " myemailATserverDOTcom $2
+    git config --global user.email $2
+    break
+done
+
+while true; do
+    read -p "Please set your core editor: " vim $3
+    git config --global core.editor $1
+    break
+done
+while true; do
+    read -p "Please set your diff app: " vimdiff $4
+    git config --global merge.tool $4
+    break
+done
+git config --list
+echo "Here you are an excellent Github cheatsheet https://raw.githubusercontent.com/hbons/git-cheat-sheet/master/preview.png You can also access as gitsheet"
+echo "If you get stuck, run ‘git branch -a’ and it will show you exactly what’s going on with your branches. You can see which are remotes and which are local."
+echo "Do not forget to add a newsshkey or clipboard your mysshkey or mylastsshkey (if you switchsshkey before) and paste it on Settings -> New SSH key and paste it there." 
+
+
+sh -c "curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh"
+echo "installed vimcr"
+wget https://raw.githubusercontent.com/Russell91/sshrc/master/sshrc && sudo chmod -R 600 sshrc && chmod +x sshrc && sudo mv sshrc /usr/local/bin
+echo "ssh bashcr vimcr portability installed"
+
+
+#Ethr
+sudo apt-get install gfortran aptitude libghc-curl-dev -y 
+sudo aptitude install r-base -y
+sudo aptitude install openjdk-7-source -y
+sudo updatedb
+sudo wget https://cran.r-project.org/src/base-prerelease/R-latest.tar.gz
+tar -xvzf R-latest.tar.gz
+sudo rm -r -f R-latest.tar.gz
+cd R-beta
+sudo ./configure
+sudo make
+sudo make install
+bash <(curl -L https://install-geth.ethereum.org)
+#geth --rpc --rpccorsdomain localhost --autodag console 2>>/dev/tty
+sudo R
+install.packages("EthR", repos = "https://github.com/BSDStudios/ethr", type="source")
+install.packages("plyr") -21
+install.packages("dplyr") -21
+install.packages("httr")
+install.packages("igraph")
+install.packages("gmp")
+exit() -n
+
+#Text Edition Tools
+sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y 
+sudo apt-get update -y
+sudo apt-get install vim -y
+git clone https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+sudo apt-get install gedit -y
+sudo apt-get install sublime-text-installer -y
+sudo apt-get install libreoffice -y
+sudo apt-get install build-essential python-dev python-pip python-setuptools libxml2-dev libxslt1-dev zlib1g-dev -y
+pip install --upgrade pip
+virtualenv rtd
+cd rtd
+source bin/activate
+git clone https://github.com/rtfd/readthedocs.org.git
+cd readthedocs.org
+sudo pip install -r requirements.txt
+sudo easy_install --upgrade pip
+sudo pip uninstall pyopenssl
+sudo pip install mozdownload
+sudo ./manage.py migrate
+sudo ./manage.py createsuperuser
+sudo ./manage.py collectstatic
+#sudo ./manage.py loaddata test_data
+#sudo ./manage.py update_repos pip
+sudo ./manage.py runserver
+firefox  http://127.0.0.1:8000
