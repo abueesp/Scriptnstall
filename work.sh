40412e70bd7d1f23ed4697619531a6d028b7af5e
Alfredo Bueno <abueesp@gmail.com>
2016-05-15 11:24:08 -0400

    Update work.sh

--- work.sh
+++ work.sh
@@ -220,9 +220,43 @@ sudo rm -r gpu
 
 ##Emacs
 
+#!/bin/bash
+
+# Build latest version of Emacs, version management with stow
+# OS: Ubuntu 14.04 LTS
+# version: 24.5
+# Toolkit: lucid
+
+# Warning, use updated version of this script in: https://github.com/favadi/build-emacs
+sudo rm -r /usr/local/stow
+set -e
+
 readonly version="24.5"
-sudo rm emacs-"$version".tar.xz
-sudo rm -r emacs-"$version"
+
+# install dependencies
+sudo apt-get install -y stow build-essential libx11-dev xaw3dg-dev \
+     libjpeg-dev libpng12-dev libgif-dev libtiff5-dev libncurses5-dev \
+     libxft-dev librsvg2-dev libmagickcore-dev libmagick++-dev \
+     libxml2-dev libgpm-dev libghc-gconf-dev libotf-dev libm17n-dev \
+     libgnutls-dev
+
+# download source package
+if [[ ! -d emacs-"$version" ]]; then
+   wget http://ftp.gnu.org/gnu/emacs/emacs-"$version".tar.xz
+   tar xvf emacs-"$version".tar.xz
+fi
+
+# buil and install
+sudo mkdir -p /usr/local/stow
+cd emacs-"$version"
+./configure \
+    --with-xft \
+    --with-x-toolkit=lucid
+make
+sudo rm -r /usr/local/stow
+sudo make install prefix=/usr/local/stow/emacs-"$version" && cd /usr/local/stow
+sudo rm /usr/local/share/info/dir
+sudo stow emacs-"$version" 
 #spacemacs
 sudo git clone http://github.com/syl20bnr/spacemacs ~/.emacs.d
 sudo mv $(sudo find / -name .emacs.d) .emacs.d.bak
