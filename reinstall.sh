90d079648889d750fe131e0ef6b89fad26ec0be4
Alfredo Bueno <abueesp@gmail.com>
2016-05-15 05:27:50 +0200

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -95,17 +95,148 @@ sudo apt-get install tmux -y
 sudo apt-get install htop -y
 sudo apt-get install pandoc -y
 
-##GPG
+##GPU
+
+mkdir gpu
+cd gpu
+wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2
+wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2.sig
+sha1 = $(sha1sum **tar.bz2)
+if [ $sha1 "c40015ed88bf5f50fa58d02252d75cf20b858951" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+gpg --verify **.sig **.bz2
+tar xvjf **.tar.bz2
+cd **
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+sudo rm **.bz2 && sudo rm **.sig
+
+wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2
+wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2.sig
+sha1 = $(sha1sum **tar.bz2)
+if [ $sha1 "f840b737faafded451a084ae143285ad68bbfb01" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+gpg --verify **.sig **.bz2
+tar xvjf **.tar.bz2
+cd **
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+sudo rm **.bz2 && sudo rm **.sig
+
+
+wget https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2
+wget https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2.sig
+sha1 = $(sha1sum **tar.bz2)
+if [ $sha1 "bc84945400bd1cabfd7b8ba4e20e71082f32bcc9" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+gpg --verify **.sig **.bz2
+tar xvjf **.tar.bz2
+cd **
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+sudo rm **.bz2 && sudo rm **.sig
+
+wget https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2
+wget https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2.sig
+sha1 = $(sha1sum **tar.bz2)
+if [ $sha1 "ac1047f9764fd4a4db7dafe47640643164394db9" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+gpg --verify **.sig **.bz2
+tar xvjf **.tar.bz2
+cd **
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+sudo rm **.bz2 && sudo rm **.sig
+
+wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2
+wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2.sig
+sha1 = $(sha1sum **tar.bz2)
+if [ $sha1 "3bfa2a2d7521d6481850e8a611efe5bf5ed75200" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+gpg --verify **.sig **.bz2
+tar xvjf **.tar.bz2
+cd **
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+sudo rm **.bz2 && sudo rm **.sig
+
 wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.12.tar.bz2
 wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.12.tar.bz2.sig
-sha1sum gnupg**tar.bz2
-echo verifica el último número fue igual a y pulsa ENTER
-$pausee
-tar xvjf gnupg**.tar.bz2
-gpg --verify gnupg**.sig gnupg**.bz2
-cd gnupg**
+sha1 = $(sha1sum **tar.bz2)
+if [ $sha1 "3b01a35ac04277ea31cc01b4ac4e230e54b5480c" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+gpg --verify **.sig **.bz2
+tar xvjf **.tar.bz2
+cd **
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+sudo rm **.bz2 && sudo rm **.sig
 
-rm gnupg**
+wget https://www.gnupg.org/ftp/gcrypt/gpa/gpa-0.9.9.tar.bz2
+wget https://www.gnupg.org/ftp/gcrypt/gpa/gpa-0.9.9.tar.bz2.sig
+sha1 = $(sha1sum **tar.bz2)
+if [ $sha1 "1cf86c9e38aa553fdb880c55cbc6755901ad21a4" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+gpg --verify **.sig **.bz2
+tar xvjf **.tar.bz2
+cd **
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+sudo rm **.bz2 && sudo rm **.sig
+
+cd ..
+sudo rm -r gpu
+
+
 ##Fail2ban
 sudo apt-get install fail2ban -y
 sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
