34cf255461a64f91eaa2cc2a1540681ba810e7c6
Alfredo Bueno <abueesp@gmail.com>
2016-05-16 20:05:59 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -80,8 +80,8 @@ firefox https://addons.mozilla.org/firefox/downloads/file/271802/no_more_install
 sudo apt-get install libgtk2.0-dev
 mkdir gpg
 cd gpg
-wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.22.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "c40015ed88bf5f50fa58d02252d75cf20b858951" ]
 then
@@ -91,7 +91,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd libgp**
 ./configure
 sudo make
@@ -99,8 +99,8 @@ sudo make install
 cd ..
 sudo rm **.bz2 && sudo rm **.sig && sudo rm -r libgp**
 
-wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.7.0.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "f840b737faafded451a084ae143285ad68bbfb01" ]
 then
@@ -110,7 +110,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd libgcr**
 ./configure
 sudo make
@@ -118,8 +118,8 @@ sudo make install
 cd ..
 sudo rm **.bz2 && sudo rm **.sig && sudo rm -r libgcr**
 
-wget https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/libksba/libksba-1.3.4.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "bc84945400bd1cabfd7b8ba4e20e71082f32bcc9" ]
 then
@@ -129,7 +129,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd libks**
 ./configure
 sudo make
@@ -137,8 +137,8 @@ sudo make install
 cd ..
 sudo rm **.bz2 && sudo rm **.sig && sudo rm -r libks**
 
-wget https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.4.2.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "ac1047f9764fd4a4db7dafe47640643164394db9" ]
 then
@@ -148,7 +148,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd libas**
 ./configure
 sudo make
@@ -156,8 +156,8 @@ sudo make install
 cd ..
 sudo rm **.bz2 && sudo rm **.sig && sudo rm -r libas**
 
-wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/npth/npth-1.2.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "3bfa2a2d7521d6481850e8a611efe5bf5ed75200" ]
 then
@@ -167,7 +167,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd npth**
 ./configure
 sudo make
@@ -175,8 +175,8 @@ sudo make install
 cd ..
 sudo rm **.bz2 && sudo rm **.sig && sudo npth**
 
-wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.12.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.12.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.12.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.12.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "3b01a35ac04277ea31cc01b4ac4e230e54b5480c" ]
 then
@@ -186,7 +186,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd gnupg**
 ./configure
 sudo make
@@ -194,8 +194,8 @@ sudo make install
 cd ..
 sudo rm **.bz2 && sudo rm **.sig && sudo rm -r gnupg**
 
-wget https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.6.0.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.6.0.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.6.0.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.6.0.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "f840b737faafded451a084ae143285ad68bbfb01" ]
 then
@@ -205,7 +205,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd gpgm**
 ./configure
 sudo make
@@ -213,8 +213,8 @@ sudo make install
 cd ..
 sudo rm **.bz2 && sudo rm **.sig && sudo rm -r gpgm**
 
-wget https://www.gnupg.org/ftp/gcrypt/gpa/gpa-0.9.9.tar.bz2
-wget https://www.gnupg.org/ftp/gcrypt/gpa/gpa-0.9.9.tar.bz2.sig
+sudo wget https://www.gnupg.org/ftp/gcrypt/gpa/gpa-0.9.9.tar.bz2
+sudo wget https://www.gnupg.org/ftp/gcrypt/gpa/gpa-0.9.9.tar.bz2.sig
 sha1 = $(sha1sum **tar.bz2)
 if [ $sha1 "1cf86c9e38aa553fdb880c55cbc6755901ad21a4" ]
 then
@@ -224,7 +224,7 @@ else
     break
 fi
 gpg --verify **.sig **.bz2
-tar xvjf **.tar.bz2
+sudo tar xvjf **.tar.bz2
 cd gpa**
 ./configure
 sudo make
