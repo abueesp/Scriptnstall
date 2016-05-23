40caa43475df79f0afcdb5a828c01c8231c027e5
Alfredo Bueno <abueesp@gmail.com>
2016-05-24 01:58:26 +0200

    Update mobile.sh

--- mobile.sh
+++ mobile.sh
@@ -35,12 +35,59 @@
 #cd $OUT
 
 ##INSTALL
-sudo apt-get install android-tools-adb android-tools-fastboot mtp-tools mtpfs
+##Build
+#sudo apt-get install android-tools-adb android-tools-fastboot bison build-essential curl flex git gnupg gperf libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev libxml2 libxml2-utils lzop maven openjdk-7-jdk openjdk-7-jre pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev g++-multilib gcc-multilib lib32ncurses5-dev lib32readline-gplv2-dev lib32z1-dev -y
+#mkdir -p ~/bin
+#mkdir -p ~/android/system/vendor/cm
+#curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+#chmod a+x ~/bin/repo
+#echo '\n# set PATH so it includes user private bin if it exists
+#\nif [ -d "$HOME/bin" ] ; then
+#\n    PATH="$HOME/bin:$PATH"
+#\nfi' >> ~/bin/.profile
+#cd ~/android/system/
+#repo init -u https://github.com/CyanogenMod/android.git -b cm-13.0
+#repo sync
+#source build/envsetup.sh
+#breakfast z3c
+#cd ..
+#cd ..
+#cd ~/android/system/vendor/cm
+#./get-prebuilts
+#cd ..
+#cd ..
+#cd ..
+#cd ..
+#cd ~/android/system/device/sony/z3c
+#./extract-files.sh
+#cd ..
+#cd ..
+#cd ..
+#cd ..
+#cd ..
+#echo "\nexport USE_CCACHE=1" >> sudo /etc/bash.bashrc
+
+#chroot 
+#brunch z3c
+#cd $OUT
+
+##INSTALL
+sudo apt-get install android-tools-adb android-tools-fastboot mtp-tools mtpfs libusb-dev gmtp -y
 sudo lsusb
+ls -lah /usr/lib/libmtp*
+wget http://downloads.sourceforge.net/project/libmtp/libmtp/1.1.11/libmtp-1.1.11.tar.gz
+wget http://downloads.sourceforge.net/project/libmtp/libmtp/1.1.11/libmtp-1.1.11.tar.gz.asc
+gpg --verify libmtp**.asc
+tar xvf libmtp**.tar.gz
+cd libmtp**/
+./configure --prefix=/usr
+make
+sudo make install
+sudo cp 69-libmtp.rules /etc/udev/rules.d
 sudo mtp-detect | grep idVendor
 sudo mtp-detect | grep idProduct
-echo 'write this string into the file: SUBSYSTEM=="usb", ATTR{idVendor}=="VENDORID", ATTR{idProduct}=="PRODUCTID", MODE="0666" and save. Replace VENDORID with the idVendor you had noted down earlier. Similarly, replace PRODUCTID with the idProduct you had noted down. In my case, they were 04e8 and 685c respectively, but they might have been different for you.'
-gksu gedit /etc/udev/rules.d/51-android.rules
+echo 'write this string into the file: SUBSYSTEM=="usb", ATTR{idVendor}=="VENDORID", ATTR{idProduct}=="PRODUCTID", MODE="0666" and save. Replace VENDORID with the idVendor you had noted down earlier. Similarly, replace PRODUCTID with the idProduct you had noted down. In my case, they were 0fce and 01bb respectively, but they might have been different for you.'
+sudo gedit /etc/udev/rules.d/51-android.rules
 sudo sed -i 's/#user_allow_other/user_allow_other/g' /etc/fuse.conf
 NOW = date
 
