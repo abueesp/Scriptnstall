78ef139dba962eed4336aac775352e0cb56e5cfc
Abueesp <abueesp@gmail.com>
2016-06-26 20:44:57 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#Prntscreensound
+sudo mv /usr/share/sounds/freedesktop/stereo/camera-shutter.oga /usr/share/sounds/freedesktop/stereo/camera-shutter-disabled.oga
 #Bluetooth
 sudo sed -i 's/InitiallyPowered = true/InitiallyPowered = false/g' /etc/bluetooth/main.conf
 rfkill block bluetooth
