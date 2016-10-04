663611447fe67063cdf8884bb4efaedd89b59e80
Abueesp <abueesp@gmail.com>
2016-10-04 14:06:12 +0200

    Update ethusec.sh

--- ethusec.sh
+++ ethusec.sh
@@ -153,7 +153,7 @@ fi
 
 ##Parity
 echo "Parity"
-curl https://get.parity.io -Lk
+bash <(curl https://get.parity.io -Lk)
 echo "alias paritysheet='firefox -new-tab https://ethcore.github.io/parity/ethcore/index.html'" >> sudo /etc/bash.bashrc
 
 read -p "Do you want to open Myetherwallet? " REPLY
