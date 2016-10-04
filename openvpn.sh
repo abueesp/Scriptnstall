97ceda796d839b63bd2f890e8ca65fee7693272e
Abueesp <abueesp@gmail.com>
2016-10-04 16:22:58 +0200

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -13,6 +13,7 @@ gpg --keyserver pgp.mit.edu --recv 198D22A3
 echo "installing openvpn-2.3.11"
 mkdir ovpn
 cd ovpn
+sudo apt-get install libssl1.0.0 libssl-dev liblzo2-dev libpam0g-dev -y
 sudo wget https://swupdate.openvpn.org/community/releases/openvpn-2.3.11.tar.gz
 sudo wget https://swupdate.openvpn.org/community/releases/openvpn-2.3.11.tar.gz.asc
 gpg --verify **.asc
