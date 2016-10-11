712164a8c6f3fc6d46b2070cc957db1cacf7980f
Abueesp <abueesp@gmail.com>
2016-10-12 01:24:12 +0200

    Update selenium.sh

--- shs/selenium.sh
+++ shs/selenium.sh
@@ -1,3 +1,4 @@
+sudo pip install selenium-webdriver
 ##Java http://selenium-release.storage.googleapis.com/3.0-beta4/selenium-java-3.0.0-beta4.zip
 sudo npm install selenium-webdriver ##JS
 sudo gem install selenium-webdriver #RoR 
@@ -8,5 +9,8 @@ cd bin
 sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
 sudo tar -zxvf geckodriver**.tar.gz
 export PATH=$PATH:/bin/geckodriver
+sudo su
+echo 'export PATH=$PATH:/bin/geckodriver' >> /etc/bash.bashrc
+exit
 sudo rm geckodriver**.tar.gz
 
