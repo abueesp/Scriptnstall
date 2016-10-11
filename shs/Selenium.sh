3440ce2a22bd7a4bc62d7445c09dba6f723e7fe5
Abueesp <abueesp@gmail.com>
2016-10-11 23:51:29 +0200

    Create Selenium.sh

---
+++ shs/Selenium.sh
@@ -0,0 +1,12 @@
+##Java http://selenium-release.storage.googleapis.com/3.0-beta4/selenium-java-3.0.0-beta4.zip
+sudo npm install selenium-webdriver ##JS
+sudo gem install selenium-webdriver #RoR 
+
+
+#driver for mozilla and chrome
+cd bin
+sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
+sudo tar -zxvf geckodriver**.tar.gz
+export PATH=$PATH:/bin/geckodriver
+sudo rm geckodriver**.tar.gz
+
