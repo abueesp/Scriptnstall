b0837a8dd836118d203d82523146fb0a75c95458
Abueesp <abueesp@gmail.com>
2016-10-12 01:27:38 +0200

    Update selenium.sh

--- shs/selenium.sh
+++ shs/selenium.sh
@@ -8,9 +8,14 @@ sudo gem install selenium-webdriver #RoR
 cd bin
 sudo wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
 sudo tar -zxvf geckodriver**.tar.gz
+sudo wget http://chromedriver.storage.googleapis.com/2.24/chromedriver_linux64.zip
+sudo unzip chromedriver**.zip
 sudo su
+export PATH=$PATH:/bin/chromedriver
+echo 'export PATH=$PATH:/bin/chromedriver' >> /etc/bash.bashrc
 export PATH=$PATH:/bin/geckodriver
 echo 'export PATH=$PATH:/bin/geckodriver' >> /etc/bash.bashrc
 exit
 sudo rm geckodriver**.tar.gz
+sudo rm chromedriver**.zip
 
