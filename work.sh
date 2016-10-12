1da0b744f89ac89bc888f3abeecc423c5cff3add
Abueesp <abueesp@gmail.com>
2016-10-12 22:11:43 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -89,8 +89,8 @@ sudo apt-get install firefox -y
 firefox https://addons.mozilla.org/firefox/downloads/file/271802/no_more_install_delay-3.0-fx+sm+fn+tb.xpi
 
 #youtube-dl
-sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
-sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/bin/youtube-dl
+sudo chmod a+rx /usr/bin/youtube-dl
 
 ##GNUPG
 sudo apt-get install libgtk2.0-dev -y
