06556ca9cfa881c732bab9ff0e9f72a10b873623
Abueesp <abueesp@gmail.com>
2016-10-12 22:09:24 +0200

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -109,6 +109,10 @@ sudo apt-get install pandoc -y
 sudo apt-get install vnstat -y
 sudo apt-get install duplicity deja-dup -y
 
+#youtube-dl
+sudo wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+
 ##GNUPG
 sudo apt-get install libgtk2.0-dev -y
 sudo mkdir gpg
