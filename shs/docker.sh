59efb687b29c8740fc56b8f7079a174a6959712a
Abueesp <abueesp@gmail.com>
2016-10-13 19:52:07 +0200

    Update docker.sh

--- shs/docker.sh
+++ shs/docker.sh
@@ -4,5 +4,10 @@ sudo usermod -aG docker $USER
 sudo apt-get purge apparmor -y
 sudo rm -rf /etc/apparmor.d/
 sudo rm -rf /etc/apparmor
+mkdir docker
 git clone https://github.com/CISOfy/lynis
+git clone https://github.com/konstruktoid/hardening.git
+cd hardening
+sudo gedit ubuntu.cfg
+sudo bash ubuntu.sh
 
