487bf895e2d73232467e8c489adb8d094fcbcf0e
Abueesp <abueesp@gmail.com>
2016-10-13 20:16:15 +0200

    Update ethusec.sh

--- ethusec.sh
+++ ethusec.sh
@@ -162,7 +162,6 @@ sudo rm geth**.tar.gz
 read -p "Parity. Copy and paste this bash <(curl https://get.parity.io -Lk)" pause
 $pause
 sudo sed -i  "alias paritysheet='firefox -new-tab https://ethcore.github.io/parity/ethcore/index.html'" /etc/bash.bashrc
-
 #Connect to ethstats
 git clone https://github.com/cubedro/eth-net-intelligence-api
 cd eth-net-intelligence-api
@@ -172,3 +171,12 @@ sudo mv app.json.example app.json
 echo "a38e1e50b1b82fa"
 gedit app.json
 sudo pm2 start app.json
+#Miner
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install ethminer
+parity account new
+read "Introduce your coinbase address" coinbase
+parity --author $coinbase
+sudo apt-get install xterm
+ethminer -G --opencl-device 0 #use --notify-work URL,URL to set urls to push work notifications 
