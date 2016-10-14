881b80bab252c050395d96ff4cca93d8533c7b90
Abueesp <abueesp@gmail.com>
2016-10-14 21:00:32 +0200

    Create apparmor.sh

---
+++ shs/apparmor.sh
@@ -0,0 +1,26 @@
+#AppArmor 
+sudo apt-get install apparmor apparmor-utils auditd -y
+sudo apparmor_status
+git clone git://git.launchpad.net/apparmor-profiles
+uname -a
+cd apparmor-profiles
+cd ubuntu && ls
+read -p "Select your version: " version
+cd $version
+sudo mkdir /etc/apparmorprofiles
+sudo cp -r apparmor-profiles/ubuntu/14.04/** /etc/apparmorprofiles/
+sudo mkdir /etc/apparmor.d/cache
+sudo /etc/init.d/apparmor restart
+sudo aa-enforce  /etc/apparmor.d/* #enforce mode log, report and auditd
+#sudo aa-complain /usr/sbin/mysqld #complain mode only log
+sudo auditd -s enable
+sudo rm -r apparmor-profiles
+sudo invoke-rc.d apparmor start
+
+#disableprofile
+#sudo ln -s /etc/apparmor.d/bin.ping /etc/apparmor.d/disable/
+#sudo apparmor_parser -R /etc/apparmor.d/bin.ping
+
+#enableprofile
+#sudo rm /etc/apparmor.d/disable/profile.name
+#sudo apparmor_parser -r /etc/apparmor.d/profile.name
