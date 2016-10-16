702a44dad3cb9983b2fb2d89e935db9819b4d4fc
Abueesp <abueesp@gmail.com>
2016-10-16 02:09:39 +0200

    Update docker.sh

--- shs/docker.sh
+++ shs/docker.sh
@@ -55,10 +55,10 @@ sudo iptables restart
 #–cap-drop=fsetid –cap-drop=fowner –cap-drop=mkdnod –cap-drop=net_raw –cap-drop=setgid –cap-drop=setuid –cap-drop=setfcap –cap-drop=setpcap –cap-drop=net_bin_service –cap-drop=sys_chroot –cap-drop=audit_write –cap-drop=audit_control –cap-drop=chown –cap-drop=audit_write –cap-drop=mac_admin –cap-drop=mac_override –cap-drop=mknod –cap-drop=setfcap setpcap –cap-drop=sys_admin –cap-drop=sys_boot –cap-drop=sys_module –cap-drop=sys_nice –cap-drop=sys_pacct –cap-drop=sys_rawio –cap-drop=sys_resource –cap-drop=sys_time –cap-drop=sys_tty_config
 #–secutity-opt=”apparmor:apparmor"
 echo 'Add this to your Dockerfile'
-echo "ROM debian:wheezy
 echo "RUN find / -perm +6000 -type f -exec chmod a-s {} \; \"
 echo "    || true"
-echo " "
+echo "RUN groupadd -r user && useradd -r -g user $USER"
+echo "USER $USER"
 cd /etc/apparmor.d/ && ls
 echo "Choose a profile and introduce the use–secutity-opt=”apparmor:YOURPROFILE" whenever you run docker"
 
@@ -87,6 +87,9 @@ version: “2“  services:    web:      build: .     ports:      - “5000:5000
 Docker build -t imagenameapp:versionapp nameofimage dockerfile. \
 docker-compose up -d \
 
+##O si la has descargado, verifícala\
+docker pull debian@sha256:shastring\
+
 #See current images \
 docker images    \
 docker-compose ps\
@@ -94,12 +97,16 @@ docker-compose ps\
 #Run a image on a container \
 docker run  -it --name myappimageforcontainer -d -p 1337:80 –lxc-conf /usr/share/lxc/config/common.seccomp -lxc-conf=”lxc.id_map = u 0 100000 65536″ -lxc-conf=”lxc.id_map = g 0 100000 65536″ –cap-drop=fsetid –cap-drop=fowner –cap-drop=mkdnod –cap-drop=net_raw –cap-drop=setgid –cap-drop=setuid –cap-drop=setfcap –cap-drop=setpcap –cap-drop=net_bin_service –cap-drop=sys_chroot –cap-drop=audit_write –cap-drop=audit_control –cap-drop=chown –cap-drop=audit_write –cap-drop=mac_admin –cap-drop=mac_override –cap-drop=mknod –cap-drop=setfcap setpcap –cap-drop=sys_admin –cap-drop=sys_boot –cap-drop=sys_module –cap-drop=sys_nice –cap-drop=sys_pacct –cap-drop=sys_rawio –cap-drop=sys_resource –cap-drop=sys_time –cap-drop=sys_tty_config \
  docker-compose run web env \
+ \
+puedes añadir las instrucciones para balance como -c 512 (normal es cpu 1024) o -m 512m, para conectar containers --icc=false --iptables, o -read-only\
+ \
 #Visita tu webapp \
 go to IP:1337 and there it is \
 #Audit \
 Audit using lynis audit dockerfile filename\
     SELinux/AppApparmor support – limit processes what resources they can access \
     Capabilities support – limit the maximum level a functions (or “roles”) a process can achieve within the container\
-    Seccomp support – allow/disallow what system calls can be used by processes\
+    Auditd/Seccomp support – allow/disallow what system calls can be used by processes\
     docker exec – no more SSH in containers for just management\
+    docker exec $INSTANCE_ID rpm -qa to OR docker exec $INSTANCE_ID dpkg -l, to see what packages are installed in a container.\
 ' && 'firefox -new-tab  https://www.cheatography.com/storage/thumb/aabs_docker-and-friends.600.jpg && firefox -new-tab https://container-solutions.com/content/uploads/2015/06/15.06.15_DockerCheatSheet_A2.pdf"
