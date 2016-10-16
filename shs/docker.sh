778e4d0b5c00a7f4364e08826ff492a48bced99d
Abueesp <abueesp@gmail.com>
2016-10-16 02:24:34 +0200

    Update docker.sh

--- shs/docker.sh
+++ shs/docker.sh
@@ -8,6 +8,17 @@ sudo rm -rf /etc/apparmor
 mkdir docker
 cd docker
 
+#SSH
+sudo apt-get install ssh -y
+newsshkey
+sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/gnome-keyring-ssh.desktop
+sudo sed -i 's/PermitRootLogin without password/PermitRootLogin no/' /etc/ssh/sshd_config #noroot
+sudo sed -i 's/Port **/Port 1022/' /etc/ssh/sshd_config #SSH PORT OTHER THAN 22, SET 1022
+sudo /etc/init.d/ssh restart
+sudo chown -R $USER:$USER .ssh
+sudo chmod -R 700 .ssh 
+sudo chmod +x .ssh
+
 #extra-tools
 sudo apt-get install go -y
 export GOPATH="$HOME/$USER/docker"
@@ -51,7 +62,7 @@ sudo iptables -P OUTPUT ACCEPT ##If you are a server change this to DROP OUTPUT 
 sudo iptables -P FORWARD DROP
 sudo iptables restart
 cd /etc/apparmor.d/ && ls
-echo "Use dockersheet for security recommendations"
+echo "Disable IPv6. Use dockersheet for a short guide and security recommendations"
 
 
 #DockerComposer
@@ -112,3 +123,63 @@ Audit using lynis audit dockerfile filename\
     docker exec – no more SSH in containers for just management\
     docker exec $INSTANCE_ID rpm -qa to OR docker exec $INSTANCE_ID dpkg -l, to see what packages are installed in a container.\
 ' && 'firefox -new-tab  https://www.cheatography.com/storage/thumb/aabs_docker-and-friends.600.jpg && firefox -new-tab https://container-solutions.com/content/uploads/2015/06/15.06.15_DockerCheatSheet_A2.pdf"
+
+
+#Extra
+
+#UFW
+sudo apt-get install gufw -y
+sudo ufw enable
+sudo ufw allow 1022/tcp
+sudo iptables -F
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 1022 -j ACCEPT
+sudo iptables -P INPUT DROP
+sudo iptables -P OUTPUT ACCEPT ##If you are a server change this to DROP OUTPUT connections by default too
+sudo iptables -P FORWARD DROP
+sudo iptables restart
+sudo service avahi-daemon stop
+sudo cupsctl -E --no-remote-any
+sudo service cups-browsed stop
+
+##psad
+service psad stop
+sudo apt-get -y install libc--clan-perl libdate-calc-perl libiptables-chainmgr-perl libiptables-parse-perl libnetwork-ipv4addr-perl libunix-syslog-perl libbit-vector-perl gcc wget -y
+wget https://cipherdyne.org/psad/download/psad-2.4.3.tar.gz
+wget https://cipherdyne.org/psad/download/psad-2.4.3.tar.gz.asc
+gpg --verify psad**.asc
+md5 = $(md5sum **tar.gz)
+if [ $md5 "5aa0d22f0bea3ba32e3b9730f78157cf" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+tar xvf psad**.gz
+cd psad**
+sudo ./install.pl
+cd
+sudo rm -r psad**
+service psad start
+
+#fwsnort
+wget http://cipherdyne.org/fwsnort/download/fwsnort-1.6.5.tar.gz
+wget https://cipherdyne.org/fwsnort/download/fwsnort-1.6.5.tar.gz.asc
+gpg --verify fwsnort**.asc
+md5 = $(md5sum **tar.gz)
+if [ $md5 "76552f820e125e97e4dfdd1ce6e3ead6" ]
+then
+    echo "PACKAGE VERIFIED"
+else
+    echo "PACKAGE NOT VERIFIED"
+    break
+fi
+tar xvf fwsnort**.gz
+cd fwsnort**
+./configure
+sudo make
+sudo make install
+cd
+sudo rm -r fwsnort**
