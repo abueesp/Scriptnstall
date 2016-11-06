f80399acf6316ed51dfa6e2bf9304eddbb7d5d40
Abueesp <abueesp@gmail.com>
2016-11-06 01:47:19 +0100

    Update work.sh

--- work.sh
+++ work.sh
@@ -55,6 +55,16 @@ sudo service cups-browsed stop
 sudo apt-get install baobab -y
 sudo apt-get install brasero -y
 sudo apt-get install tmux terminator -y
+
+sudo rm ~/tmux.conf~
+cp ~/tmux.conf ~/tmux.conf~
+wget https://raw.githubusercontent.com/abueesp/Scriptnstall/master/tmux.conf -O ~/tmux.conf
+tmux source-file ~/tmux.conf
+
+git clone git://github.com/zolrath/wemux.git /usr/local/share/wemuxure to use the full path.
+ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux
+cp /usr/local/share/wemux/wemux.conf.example /usr/local/etc/wemux.conf
+
 sudo apt-get install traceroute -y
 sudo apt-get install zsh -y
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
