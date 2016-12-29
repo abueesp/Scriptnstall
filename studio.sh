11378ce60f8c2861cb6cf8383fee4004a32bc066
Abueesp <abueesp@gmail.com>
2016-12-29 09:02:22 +0100

    Update studio.sh

--- studio.sh
+++ studio.sh
@@ -80,3 +80,26 @@ Description=Open folder as root
 MimeType=inode;application/x-sh;application/x-executable;
 Exec=gksudo pantheon-files -d %U
 Gettext-Domain=pantheon-files" >> sudo /usr/share/contractor/Open_as_admin.contract
+
+
+##DEEPDREAMER
+sudo apt-get install build-essential git ipython ipython-notebook -y
+cd ~
+git clone https://github.com/BVLC/caffe.git
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev python python-dev python-scipy python-setuptools python-numpy python-pip libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler libatlas-dev libatlas-base-dev libatlas3-base libatlas-test -y
+sudo apt-get install --no-install-recommends libboost-all-dev -y
+sudo pip install --upgrade pip
+sudo pip install --upgrade numpy
+cd ~/caffe
+cp Makefile.config.example Makefile.config
+echo " If you're not using CUDA, then you'll want CPU only mode. Edit Makefile.config and uncomment LINE8: CPU_ONLY := 1 LINE 37: BLAS_INCLUDE := /opt/intel/composer_xe_2011_sp1.10.319/mkl/include LINE 38: BLAS_LIB := /opt/intel/composer_xe_2011_sp1.10.319/mkl/lib/intel64" 
+gedit Makefile.config
+make all -j$(nproc)
+make test -j$(nproc)
+make runtest -j$(nproc)
+make pycaffe -j$(nproc)
+cd ~
+git clone https://github.com/google/deepdream.git
+wget -P ~/caffe/models/bvlc_googlenet http://dl.caffe.berkeleyvision.org/bvlc_googlenet.caffemodel
+cd ~/deepdream
+ipython notebook ./dream.ipynb
