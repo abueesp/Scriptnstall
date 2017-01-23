62c9c83e40dd146150f28ed0831ed2701297ff7b
Abueesp <abueesp@gmail.com>
2017-01-23 18:56:04 +0000

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -259,7 +259,7 @@ sudo apt-get build-dep virtualbox
 sudo apt-get -f install -y
 while true; do
     read -p "Please introduce introduce OS Ubuntu 16.10 ('yaketty_amd64') Ubuntu 16.04 ('xenial_amd64') Ubuntu 15.10 ('wily_i386') or Ubuntu 14.04 ('trusty_amd64') / 14.10 Utopic/ 15.04 Vivid: " OS $OS
-    sudo wget http://download.virtualbox.org/virtualbox/5.1.10/virtualbox-5.1_5.1.10-112026~Ubuntu~$OS.deb
+    wget http://download.virtualbox.org/virtualbox/5.1.14/virtualbox-5.1_5.1.14-112924~Ubuntu~$OS.deb
     break
 done
 sudo dpkg -i virtualbox**.deb
