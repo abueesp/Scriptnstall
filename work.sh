dccd7f5c1d5f0dc6f58272e571ae492a1431cec4
Abueesp <abueesp@gmail.com>
2017-05-22 06:29:17 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -904,4 +904,21 @@ wget https://addons.mozilla.org/firefox/downloads/latest/certificate-patrol/addo
 wget https://addons.mozilla.org/firefox/downloads/latest/perspectives/addon-7974-latest.xpi #perspectivenetworknotaries
 cd ..
 
+mkdir bctools
+wget http://phodd.net/gnu-bc/code/array.bc
+wget http://phodd.net/gnu-bc/code/collatz.bc    
+wget http://phodd.net/gnu-bc/code/digits.bc    
+wget http://phodd.net/gnu-bc/code/funcs.bc   
+wget http://phodd.net/gnu-bc/code/interest.bc      
+wget http://phodd.net/gnu-bc/code/melancholy.bc      
+wget http://phodd.net/gnu-bc/code/primes.bc      
+wget http://phodd.net/gnu-bc/code/thermometer.bc      
+wget http://phodd.net/gnu-bc/code/cf.bc       
+wget http://phodd.net/gnu-bc/code/complex.bc     
+wget http://phodd.net/gnu-bc/code/factorial.bc      
+wget http://phodd.net/gnu-bc/code/intdiff.bc      
+wget http://phodd.net/gnu-bc/code/logic.bc      
+wget http://phodd.net/gnu-bc/code/output_formatting.bc      
+wget http://phodd.net/gnu-bc/code/rand.bc
+
 EOF
