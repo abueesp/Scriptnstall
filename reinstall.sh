123e015f0649cb96a15d7beba3d4b9f118b719e0
Abueesp <abueesp@gmail.com>
2017-05-31 16:26:49 +0000

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -815,6 +815,26 @@ echo "alias iron='/bin/iron/./chrome'" | tee -a ~/.bashrc
 /bin/iron/./chrome https://chrome.google.com/webstore/detail/autoform/fdedjnkmcijdhgbcmmjdogphnmfdjjik
 /bin/iron/./chrome https://chrome.google.com/webstore/detail/m-i-m/jlppachnphenhdidmmpnbdjaipfigoic
 
+### Calc Tools ###
+mkdir bctools
+cd bctools
+wget http://phodd.net/gnu-bc/code/array.bc
+wget http://phodd.net/gnu-bc/code/collatz.bc    
+wget http://phodd.net/gnu-bc/code/digits.bc    
+wget http://phodd.net/gnu-bc/code/funcs.bc   
+wget http://phodd.net/gnu-bc/code/interest.bc      
+wget http://phodd.net/gnu-bc/code/melancholy.bc      
+wget http://phodd.net/gnu-bc/code/primes.bc      
+wget http://phodd.net/gnu-bc/code/thermometer.bc      
+wget http://phodd.net/gnu-bc/code/cf.bc       
+wget http://phodd.net/gnu-bc/code/complex.bc     
+wget http://phodd.net/gnu-bc/code/factorial.bc      
+wget http://phodd.net/gnu-bc/code/intdiff.bc      
+wget http://phodd.net/gnu-bc/code/logic.bc      
+wget http://phodd.net/gnu-bc/code/output_formatting.bc      
+wget http://phodd.net/gnu-bc/code/rand.bc
+cd ..
+
 sudo apt-get autoremove -y
 
 EOF
