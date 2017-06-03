73b5c5c9f374a1cdfa9c670b157113ab5523465e
Abueesp <abueesp@gmail.com>
2017-06-03 07:54:10 +0000

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -42,7 +42,7 @@ sudo gpg2 --delete-key 198D22A3 -y
 sudo chmod 600 /home/node/.gnupg/pubring.gpg
 
 ## VyprVPN ##
-read -p "Do you know that Vyprvpn trust on Cisco concentrators and 'they cannot give you more info about, but trust us, your information is secure with us'? Are you sure you want to install it? They use vpnc, vpnc-scripts and network-manager-vpnc Cisco 3000; which also use the broken SHA1; and Chameleon scramble protocol is -oh surprise- well recognized by the Chinese government? Better try https://www.privateinternetaccess.com/pages/buy-vpn/"
+read -p "Do you know that Vyprvpn keeps logs for 30 days and trust on Cisco concentrators and 'they cannot give you more info about, but trust us, your information is secure with us'? Are you sure you want to install it? They use vpnc, vpnc-scripts and network-manager-vpnc Cisco 3000; which also use the broken SHA1; and Chameleon scramble protocol is -oh surprise- well recognized by the Chinese government? Better try https://www.privateinternetaccess.com/pages/buy-vpn/"
 sudo apt-get install vpnc vpnc-scripts network-manager-vpnc -y 
 sudo wget https://support.goldenfrog.com/hc/article_attachments/212490988/vyprvpn-linux-cli-1.7.amd64.deb
 sudo dpkg -i vyprvpn*.deb
