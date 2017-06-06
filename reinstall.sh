11d21c37d61f3442f388f10bcdc496cd7b8555cb
Abueesp <abueesp@gmail.com>
2017-06-07 00:33:56 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -88,43 +88,48 @@ sudo service avahi-daemon stop
 sudo cupsctl -E --no-remote-any
 sudo service cups-browsed stop
 
+### GNU GPG 2 ###
+SCRIPTPLACE=https://raw.githubusercontent.com/abueesp/Scriptnstall/master
+wget $SCRIPTPLACE/shs/gpg2.sh
+source /gpg2.sh
+
 ### OpenSSL ###
 OPENSSLVERSION=1.1.0f
 wget https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
 wget https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz.asc
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 8657ABB260F056B1E5190839D9C4D26D0E604491 #Matt Caswell
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 5B2545DAB21995F4088CEFAA36CEE4DEB00CFE33 #Mark J. Cox
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 8657ABB260F056B1E5190839D9C4D26D0E604491 #Matt Caswell
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 5B2545DAB21995F4088CEFAA36CEE4DEB00CFE33 #Mark J. Cox
 #Viktor Dukhovni (no PGP)
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 62605AA4334AF9F0DDE5D349D3577507FA40E9E2 #Dr. Stephen Henson
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys C1F33DD8CE1D4CC613AF14DA9195C48241FBF7DD #Tim Hudson
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 0A77335AADE74E6BB36CAD8ADFAB592ABDD52F1C #Lutz Jänicke	
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 62605AA4334AF9F0DDE5D349D3577507FA40E9E2 #Dr. Stephen Henson
+gpg --keyserver hkp://keys.gnupg.net --recv-keys C1F33DD8CE1D4CC613AF14DA9195C48241FBF7DD #Tim Hudson
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 0A77335AADE74E6BB36CAD8ADFAB592ABDD52F1C #Lutz Jänicke	
 #Emilia Käsper (no PGP)
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 765655DE62E396FF2587EB6C4F6DE1562118CF83 #Ben Laurie
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys FEAB1FB2653717429B0B894F431711F76D1892F5 #Steve Marquess
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 7953AC1FBC3DC8B3B292393ED5E9E43F7DF9EE8C #Richard Levitte
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 0xAA589DAC5A6A9B85 #Bodo Möller
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys B652F27F2B8D1B8DA78D7061BA6CDA461FE8E023 #Andy Polyakov
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys E5E52560DD91C556DDBDA5D02064C53641C25E5D #Kurt Roeckx
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D099684DC7C21E02E14A8AFEF23479455C51B27C #Rich Salz
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 1B3DF808C221D2A5ED74172F0833F510E18C1C32 #Geoff Thorpe
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 765655DE62E396FF2587EB6C4F6DE1562118CF83 #Ben Laurie
+gpg --keyserver hkp://keys.gnupg.net --recv-keys FEAB1FB2653717429B0B894F431711F76D1892F5 #Steve Marquess
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 7953AC1FBC3DC8B3B292393ED5E9E43F7DF9EE8C #Richard Levitte
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 0xAA589DAC5A6A9B85 #Bodo Möller
+gpg --keyserver hkp://keys.gnupg.net --recv-keys B652F27F2B8D1B8DA78D7061BA6CDA461FE8E023 #Andy Polyakov
+gpg --keyserver hkp://keys.gnupg.net --recv-keys E5E52560DD91C556DDBDA5D02064C53641C25E5D #Kurt Roeckx
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D099684DC7C21E02E14A8AFEF23479455C51B27C #Rich Salz
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 1B3DF808C221D2A5ED74172F0833F510E18C1C32 #Geoff Thorpe
 
-gpg2 --verify openssl-$OPENSSLVERSION.tar.gz.asc openssl-$OPENSSLVERSION.tar.gz
+gpg --verify openssl-$OPENSSLVERSION.tar.gz.asc openssl-$OPENSSLVERSION.tar.gz
 if [ $? -eq 0 ]
 then
     echo "GOOD SIGNATURE"
-    gpg2 --delete-secret-and-public-keys --batch --yes 8657ABB260F056B1E5190839D9C4D26D0E604491
-    gpg2 --delete-secret-and-public-keys --batch --yes 5B2545DAB21995F4088CEFAA36CEE4DEB00CFE33
-    gpg2 --delete-secret-and-public-keys --batch --yes 62605AA4334AF9F0DDE5D349D3577507FA40E9E2
-    gpg2 --delete-secret-and-public-keys --batch --yes C1F33DD8CE1D4CC613AF14DA9195C48241FBF7DD
-    gpg2 --delete-secret-and-public-keys --batch --yes 0A77335AADE74E6BB36CAD8ADFAB592ABDD52F1C
-    gpg2 --delete-secret-and-public-keys --batch --yes 765655DE62E396FF2587EB6C4F6DE1562118CF83
-    gpg2 --delete-secret-and-public-keys --batch --yes FEAB1FB2653717429B0B894F431711F76D1892F5
-    gpg2 --delete-secret-and-public-keys --batch --yes 7953AC1FBC3DC8B3B292393ED5E9E43F7DF9EE8C
-    gpg2 --delete-secret-and-public-keys --batch --yes 0xAA589DAC5A6A9B85
-    gpg2 --delete-secret-and-public-keys --batch --yes B652F27F2B8D1B8DA78D7061BA6CDA461FE8E023
-    gpg2 --delete-secret-and-public-keys --batch --yes E5E52560DD91C556DDBDA5D02064C53641C25E5D
-    gpg2 --delete-secret-and-public-keys --batch --yes D099684DC7C21E02E14A8AFEF23479455C51B27C
-    gpg2 --delete-secret-and-public-keys --batch --yes 1B3DF808C221D2A5ED74172F0833F510E18C1C32
+    gpg --delete-secret-and-public-keys --batch --yes 8657ABB260F056B1E5190839D9C4D26D0E604491
+    gpg --delete-secret-and-public-keys --batch --yes 5B2545DAB21995F4088CEFAA36CEE4DEB00CFE33
+    gpg --delete-secret-and-public-keys --batch --yes 62605AA4334AF9F0DDE5D349D3577507FA40E9E2
+    gpg --delete-secret-and-public-keys --batch --yes C1F33DD8CE1D4CC613AF14DA9195C48241FBF7DD
+    gpg --delete-secret-and-public-keys --batch --yes 0A77335AADE74E6BB36CAD8ADFAB592ABDD52F1C
+    gpg --delete-secret-and-public-keys --batch --yes 765655DE62E396FF2587EB6C4F6DE1562118CF83
+    gpg --delete-secret-and-public-keys --batch --yes FEAB1FB2653717429B0B894F431711F76D1892F5
+    gpg --delete-secret-and-public-keys --batch --yes 7953AC1FBC3DC8B3B292393ED5E9E43F7DF9EE8C
+    gpg --delete-secret-and-public-keys --batch --yes 0xAA589DAC5A6A9B85
+    gpg --delete-secret-and-public-keys --batch --yes B652F27F2B8D1B8DA78D7061BA6CDA461FE8E023
+    gpg --delete-secret-and-public-keys --batch --yes E5E52560DD91C556DDBDA5D02064C53641C25E5D
+    gpg --delete-secret-and-public-keys --batch --yes D099684DC7C21E02E14A8AFEF23479455C51B27C
+    gpg --delete-secret-and-public-keys --batch --yes 1B3DF808C221D2A5ED74172F0833F510E18C1C32
 else
     echo "BAD SIGNATURE"
     exit
@@ -154,20 +159,20 @@ sudo apt-file update
 #SSH
 sudo apt-get purge ssh -y
 SSHVERSION=7.5p1
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys CE8ECB0386FF9C48
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys A2B989F511B5748F
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys A819A2D8691EF8DA
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D3E5F56B6D920D30
+gpg --keyserver hkp://keys.gnupg.net --recv-keys CE8ECB0386FF9C48
+gpg --keyserver hkp://keys.gnupg.net --recv-keys A2B989F511B5748F
+gpg --keyserver hkp://keys.gnupg.net --recv-keys A819A2D8691EF8DA
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D3E5F56B6D920D30
 wget https://mirrors.ucr.ac.cr/pub/OpenBSD/OpenSSH/portable/openssh-$SSHVERSION.tar.gz
 wget https://mirrors.ucr.ac.cr/pub/OpenBSD/OpenSSH/portable/openssh-$SSHVERSION.tar.gz.asc
 gpg2 --verify openssh-$SSHVERSION.tar.gz.asc openssh-$SSHVERSION.tar.gz
 if [ $? -eq 0 ]
 then
     echo "GOOD SIGNATURE"
-    gpg2 --delete-secret-and-public-keys --batch --yes CE8ECB0386FF9C48
-    gpg2 --delete-secret-and-public-keys --batch --yes A2B989F511B5748F
-    gpg2 --delete-secret-and-public-keys --batch --yes A819A2D8691EF8DA
-    gpg2 --delete-secret-and-public-keys --batch --yes D3E5F56B6D920D30
+    gpg --delete-secret-and-public-keys --batch --yes CE8ECB0386FF9C48
+    gpg --delete-secret-and-public-keys --batch --yes A2B989F511B5748F
+    gpg --delete-secret-and-public-keys --batch --yes A819A2D8691EF8DA
+    gpg --delete-secret-and-public-keys --batch --yes D3E5F56B6D920D30
 else
     echo "BAD SIGNATURE"
     break
@@ -226,12 +231,12 @@ man sshd_config | col -b | awk "/Ciphers/,/ClientAlive/"
 BASHVERSION=4.4
 wget https://ftp.gnu.org/gnu/bash/bash-$BASHVERSION.tar.gz
 wget https://ftp.gnu.org/gnu/bash/bash-$BASHVERSION.tar.gz.sig
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 64EA74AB
-gpg2 --output bash-$BASHVERSION.tar.gz --decrypt bash-$BASHVERSION.tar.gz.sig
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 64EA74AB
+gpg --output bash-$BASHVERSION.tar.gz --decrypt bash-$BASHVERSION.tar.gz.sig
 if [ $? -eq 0 ]
 then
     echo "GOOD SIGNATURE"
-    gpg2 --delete-secret-and-public-keys --batch --yes 64EA74AB
+    gpg --delete-secret-and-public-keys --batch --yes 64EA74AB
 else
     echo "BAD SIGNATURE"
     break
@@ -252,8 +257,8 @@ KERNELVERSION=4.11.3
 sudo apt-get install libncurses5-dev libncursesw5-dev -y
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$KERNELVERSION.tar.xz
 wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$KERNELVERSION.tar.sign  
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 647F28654894E3BD457199BE38DBBDC86092693E #Greg
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys ABAF11C65A2970B130ABE3C479BE3E4300411886 #Torvalds
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 647F28654894E3BD457199BE38DBBDC86092693E #Greg
+gpg --keyserver hkp://keys.gnupg.net --recv-keys ABAF11C65A2970B130ABE3C479BE3E4300411886 #Torvalds
 xz -cd linux-$KERNELVERSION.tar.xz | gpg2 --verify linux-$KERNELVERSION.tar.sign -
 if [ $? -eq 0 ]
 then
