e4936f9beb44a028a3ffb01659fc7d5d18b2d0a2
Abueesp <abueesp@gmail.com>
2017-08-07 20:14:00 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -542,13 +542,10 @@ cd ..
 sudo apt-get install libttspico** -y
 
 
-#Coq
+##Coq##
 COQVERSION=8.6
-
 sudo apt-get install ocaml ocaml-native-compilers camlp5 liblablgtk2-ocaml-dev liblablgtksourceview2-ocaml-dev libgtk2.0-dev -y
-
 #sudo apt-get install coq coqide -y
-
 wget https://coq.inria.fr/distrib/V$COQVERSION/files/coq-$COQVERSION.tar.gz
 tar -xvzf coq-$COQVERSION.tar.gz
 cd coq-$COQVERSION
@@ -557,11 +554,22 @@ make
 sudo make install
 rm coq-$COQVERSION.tar.gz
 rm -r coq-$COQVERSION
-
 echo "Coq test"
 coqc -v
 echo "Write down Check True to test coqide"
 coqide &
+git clone https://bitbucket.org/Peter_Sewell/lem.git
+cd lem
+make && make coq-libs
+#for OCaml   : make ocaml-libs
+#for HOL 4   : make hol-libs
+#for Isabelle: make isa-libs
+#for Coq     : make coq-libs
+#If you just want to generate the input which Lem gives to these tools, please run make libs.
+echo "Run as ~/coq-$COQVERSION/lem./lem -coq" 
+cd ..
+cd ..
+
 
 ##Dyagrams/Knowledge/Concepts/Ontology
 #MySQLWorkbench
