1f081130a940cd969590be2d203b7d48a4b37147
Abueesp <abueesp@gmail.com>
2017-09-26 17:09:28 +0200

    Update haskell.sh

--- shs/haskell.sh
+++ shs/haskell.sh
@@ -3,7 +3,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:hvr/ghc
 sudo apt-get update
 sudo apt-get install haskell-platform -y
-sudo apt-get install -y cabal-install-1.22 ghc-7.10.3
+sudo apt-get install cabal-install ghc -y
 sudo su
 export PATH=~/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH
 export PATH=$HOME/.cabal/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.3/bin:$PATH
