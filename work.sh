a597cb89401d568a855d7265799e096a3f3cfd10
Abueesp <abueesp@gmail.com>
2017-10-06 20:07:22 +0200

    Update work.sh

--- work.sh
+++ work.sh
@@ -162,7 +162,7 @@ sudo rm -r -f R-latest.tar.gz
 cd Rfolder
 sudo wget  ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.20.tar.bz2
 sudo wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre2-10.20.tar.bz2.sig
-sudo gpg2 --verify prce**sig pcre**bz2
+sudo gpg --verify prce**sig pcre**bz2
 sudo bzip2 -d pcre**bz2
 sudo tar -xvf pcre**tar
 sudo rm pcre**.bz2 pcre**.bz2.sig pcre**.tar
@@ -182,8 +182,8 @@ echo "install Geth if you want to see EthR working"
 cd
 sudo wget https://www.mpfr.org/mpfr-current/mpfr-3.1.4.zip
 sudo wget https://www.mpfr.org/mpfr-current/mpfr-3.1.4.tar.xz.asc
-gpg2 --recv-keys 98C3739D
-gpg2 --verify mpfr**.asc
+gpg --recv-keys 98C3739D
+gpg --verify mpfr**.asc
 sudo unzip mpfr**
 sudo rm -r -f mpfr**.zip
 sudo rm -r -f mpfr**.asc
@@ -217,9 +217,9 @@ sudo apt-get build-dep nodejs -y
 sudo npm build-dep npm -g
 versionnpm=v6.11.2
 sudo wget $(echo "https://nodejs.org/dist/"$versionnpm"/node-$versionnpm-linux-x64.tar.xz")
-gpg2 --keyserver pool.sks-keyservers.net --recv-keys DD8F2338BAE7501E3DD5AC78C273792F7D83545D
+gpg --keyserver pool.sks-keyservers.net --recv-keys DD8F2338BAE7501E3DD5AC78C273792F7D83545D
 sudo wget $(echo "https://nodejs.org/dist/"$versionnpm"/SHASUMS256.txt.asc")
-gpg2 --verify SHASUMS256.txt.asc
+gpg --verify SHASUMS256.txt.asc
 sudo rm SHASUM**
 sudo tar --strip-components 1 -xf /usr/local/node**.tar.xz
 sudo npm init -y
@@ -377,16 +377,16 @@ sudo rm -r -f Jaxx**.tar.gz
 ##Icecold
 git clone https://github.com/ryepdx/ethaddress.org
 cd ethaddress.org
-gpg2 --verify ryep**.asc
+gpg --verify ryep**.asc
 cd ..
 echo "alias coldstorage = 'cd /home/$USER/ethaddress.org && firefox -new-tab -url index.html'"
 git clone https://github.com/pointbiz/bitaddress.org
 cd bitaddress.org
-gpg2 --verify CHANGE**.asc
+gpg --verify CHANGE**.asc
 cd ..
 git clone https://github.com/litecoin-project/liteaddress.org
 cd liteaddress.org
-gpg2 --verify CHANGE**.asc
+gpg --verify CHANGE**.asc
 cd ..
 
 ##Electrum
@@ -442,11 +442,11 @@ echo "Public calendar servers. A calendar is simply a collection of timestamps; 
 #RVM is a command-line tool which allows you to easily install, manage, and work with multiple ruby environments from interpreters to sets of gems.
 sudo apt-get install -y git-core subversion gnupg2
 curl -sSL https://get.rvm.io | bash -s stable --rails --ruby
-sudo gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer
 curl -O https://raw.githubusercontent.com/rvm/rvm/master/binscripts/rvm-installer.asc
-sudo gpg2 --verify rvm-installer.asc 
-sudo curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
+sudo gpg --verify rvm-installer.asc 
+sudo curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 bash rvm-installer stable
 rvm list
 rvm rubies
@@ -507,7 +507,7 @@ echo "alias iron='/bin/iron/./chrome'" | tee -a ~/.bashrc
 
 wget https://ftp.gnu.org/gnu/icecat/38.8.0-gnu2/icecat-38.8.0.en-US.linux-x86_64.tar.bz2
 wget https://ftp.gnu.org/gnu/icecat/38.8.0-gnu2/icecat-38.8.0.en-US.linux-x86_64.tar.bz2.sig
-gpg2 --output icecat**tar.bz2 --decrypt icecat**tar.bz2.sig
+gpg --output icecat**tar.bz2 --decrypt icecat**tar.bz2.sig
 tar -jxvf **tar.bz2
 
 wget http://downloads.sourceforge.net/project/dooble/Version%201.56b/dooble-master.zip
@@ -530,8 +530,8 @@ export PATH=$PATH:/usr/local/go/bin
 #FTP VSFTPD
 wget https://security.appspot.com/downloads/vsftpd-3.0.3.tar.gz
 wget https://security.appspot.com/downloads/vsftpd-3.0.3.tar.gz.asc
-gpg2 --keyserver pgpkeys.mit.edu --recv-key 3C0E751C
-gpg2 --with-fingerprint vsftpd**.asc
+gpg --keyserver pgpkeys.mit.edu --recv-key 3C0E751C
+gpg --with-fingerprint vsftpd**.asc
 sudo rm vsftpd**.asc
 read "Is verified?" pause
 tar xvzf vsftpd**.tar.gz
