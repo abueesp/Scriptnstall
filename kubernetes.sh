548dd6bcec4fcb79c22e7d7d8b0da44936810243
abueesp <abueesp@gmail.com>
2017-12-07 00:07:53 +0800

    Create kubernetes.sh

---
+++ kubernetes.sh
@@ -0,0 +1,18 @@
+#Choose app
+read -p "Write down the route of your app to create it: " ROUTEAPP
+kubectl run app --image $ROUTEAPP
+#Create replicas
+read -p "Choose the number of replicas: " REPLICAS
+kubectl scale deployment app --replicas $REPLICAS
+#Open port
+read -p "Choose port: " PORT
+kubectl expose deployment app --port $PORT --type=LoadBalancer
+#Confirm deployment
+kubectl get service app
+#Confirm availability
+read -p "Introduce IP: " IP
+curl $IP
+echo "Watch your running pods using 'watch -n 1 kubectl get pods'"
+#Update app
+read -p "Write down the route of the update of your app: " UPDATER
+kubectl set image deployment app $UPDATER
