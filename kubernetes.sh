9ee02d3689a528c79d28f24d42642a0a152a405b
abueesp <abueesp@gmail.com>
2017-12-09 23:53:59 +0800

    Update kubernetes.sh

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,7 +1,15 @@
 #Install
 echo "Pick your right solution and follow the instructions to install https://kubernetes.io/docs/setup/pick-right-solution/"
+#Create a node
+echo "Create your nodes first, and then, "
+#Create daemon
+read -p "Write down the route of a daemon.yaml to create it on your nodes: " DAEMON
+kuberctl create -f $DAEMON
+#Create pod
+read -p "Write down the route of you pod.yaml to create it on your daemons: " POD
+kubectl create -f $POD
 #Choose app
-read -p "Write down the route of your app to create it: " ROUTEAPP
+read -p "Write down the route of your app to execute on your pods: " ROUTEAPP
 kubectl run app --image $ROUTEAPP
 #Create replicas
 read -p "Choose the number of replicas: " REPLICAS
@@ -18,9 +26,3 @@ echo "Watch your running pods using 'watch -n 1 kubectl get pods'"
 #Update app
 read -p "Write down the route of the update of your app: " UPDATER
 kubectl set image deployment app $UPDATER
-#Create daemon
-read -p "Write down the route of a daemon.yaml: " DAEMON
-kuberctl create -f $DAEMON
-#Create pod
-read -p "Write down the route of you pod.yaml: " POD
-kubectl create -f $POD
