3e54cb0e35ae480447a95bdbb2b92f772cfb326e
abueesp <abueesp@gmail.com>
2018-03-24 03:25:04 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,8 +1,9 @@
-#Update and Upgrade
+### Update and Upgrade ###
 sudo pacman -Syu --noconfirm 
 echo "alias pacmansheet='firefox --new-tab https://wiki.archlinux.org/index.php/Pacman/Rosetta'" | tee -a ~/.bashrc
 
-#Restoring Windows on Grub2
+
+### Restoring Windows on Grub2 ###
 sudo os-prober
 if [ $? -eq 0 ]
             then
@@ -10,13 +11,16 @@ if [ $? -eq 0 ]
             else
                 echo "No Windows installed"
             fi
-            
-#Prntscreensound
+ 
+ 
+### Prntscreensound ###
 sudo mv /usr/share/sounds/freedesktop/stereo/camera-shutter.oga /usr/share/sounds/freedesktop/stereo/camera-shutter-disabled.oga
 
-#Tor
+
+### Tor ###
 sudo pacman -S arch-install-scripts base arm --noconfirm --needed
 sudo pacman -S tor --noconfirm --needed
+
 #Run Tor as chroot
 sudo find /var/lib/tor/ ! -user tor -exec chown tor:tor {} \;
 sudo chown -R tor:tor /var/lib/tor/
@@ -67,6 +71,7 @@ TORHASH=$(echo -n $RANDOM | sha256sum)
 sudo vi -c "s/#SocksPort 9050/SocksPort $TORPORT/g" -c ":wq" /etc/tor/torrc
 sudo vi -c "s/#ControlPort 9051/#ControlPort $TORCONTROLPORT/g" -c ":wq" /etc/tor/torrc
 sudo vi -c "s/#HashedControlPassword*$/#HashedControlPassword 16:${HASH:-2}/g" -c ":wq" /etc/tor/torrc
+
 #Running Tor in a systemd-nspawn container with a virtual network interface [which is more scure than chroot]
 sudo mkdir /srv/container
 sudo mkdir /srv/container/tor-exit
@@ -78,10 +83,12 @@ echo "[Service]
 ExecStart=
 ExecStart=/usr/bin/systemd-nspawn --quiet --keep-unit --boot --link-journal=guest --network-macvlan=$INTERFACE --private-network --directory=/var/lib/container/%i
 LimitNOFILE=32768" | tee -a /etc/systemd/system/systemd-nspawn@tor-exit.service.d/tor-exit.conf
+
 #Checking conf
 machinectl login tor-exit
 systemd-nspawn@tor-exit.service
 networkctl
+
 #All DNS queries to Tor
 echo "DNSPort $TORPORT
 AutomapHostsOnResolve 1
@@ -97,10 +104,12 @@ sudo vi -c "s/#nohook resolv.conf/nohook resolv.conf/g" -c ":wq" /etc/dhcpcd.con
 sudo dnsmasq 
 echo "You can run Tor DNS queries using tor-resolve duckduckgo.com"
 tor-resolve duckduckgo.com
+
 #Pacman over Tor
 echo "XferCommand = /usr/bin/curl --socks5-hostname localhost:$TORPORT -C - -f %u > %o" | sudo tee -a /etc/pacman.conf
 
-#GPG2
+
+### GPG2 ###
 mkdir gpg2
 cd gpg2
 gpg --keyserver hkp://pgp.mit.edu --recv-keys 4F25E3B6
@@ -177,7 +186,8 @@ gpg --delete-keys 33BD3F06
 cd ..
 sudo rm -r gpg2
 
-#Virtualbox
+
+### Virtualbox ###
 sudo pacman -S virtualbox-host-modules-arch qt4 virtualbox --noconfirm --needed
 sudo gpasswd -a $USER vboxusers
 sudo /sbin/rcvboxdrv setup
@@ -200,30 +210,96 @@ echo "To insert iso additions, install first your vm"
 virtualbox
 vboxmanage storageattach work --storagectl IDE --port 0 --device 0 --type dvddrive --medium "/home/$USER/VBox**.iso"
 
-#Emacs
+
+### Emacs ###
 sudo pacman -S emacs --noconfirm --needed
 sudo pacman -S git --noconfirm --needed
-git clone http://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 cd ~/.emacs.d
-wget http://github.com/ethereum/emacs-solidity/blob/master/solidity-mode.el
+wget https://github.com/ethereum/emacs-solidity/blob/master/solidity-mode.el
 echo 'Carga los elementos de emacs con (add-to-list load-path "~/.emacs.d/") + (load "myplugin.el")' >> README
 cd ..
 
-#Tmux
+
+### Vim ###
+sudo pacman -S vim --noconfirm --needed
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+#PATHOGENFOLDER="~/.vim/build"
+#mkdir $PATHOGENFOLDER
+PATHOGENFOLDER="~/.vim_runtime/sources_non_forked/"
+echo "PATHOGENFOLDER=$PATHOGENFOLDER" | tee -a .bashrc
+echo 'alias pathogen="read -p \"Name of the plugin: \" PLUGINNAME && read -p \"Plugin Git link: \" PLUGINGIT && git clone $PLUGINGIT $PATHOGENFOLDER/$PLUGINNAME"' | tee -a .bashrc
+echo 'alias installvimplugin="pathogen"' | tee -a .bashrc
+
+git clone https://github.com/ocaml/merlin $PATHOGENFOLDER/merlin
+git clone https://github.com/OmniSharp/omnisharp-vim $PATHOGENFOLDER/omnisharp-vim && cd $PATHOGENFOLDER/omnisharp-vim && git submodule update --init --recursive && cd server && xbuild && cd
+#git clone https://github.com/rhysd/vim-crystal/ $PATHOGENFOLDER/vim-crystal
+#git clone https://github.com/venantius/vim-eastwood.git $PATHOGENFOLDER/vim-eastwood
+git clone https://github.com/rust-lang/rust.vim $PATHOGENFOLDER/rust
+git clone https://github.com/kballard/vim-swift.git $PATHOGENFOLDER/swift
+git clone --recursive https://github.com/python-mode/python-mode $PATHOGENFOLDER/python-mode
+git clone https://github.com/eagletmt/ghcmod-vim $PATHOGENFOLDER/python-mode
+git clone https://github.com/eagletmt/ghcmod-vim $PATHOGENFOLDER/ghcmod-vim
+git clone https://github.com/eagletmt/neco-ghc $PATHOGENFOLDER/neco-ghc
+
+vimfunctions(){
+echo "Tools"
+echo "ack: Search tool, :grep=:ack :grepadd=:ackadd, :lgrep=LAck, and :lgrepadd=:LAckAdd (see all options with :ack ?)"
+echo "bufexplorer: See and manage the current buffers(,o)"
+echo "mru: Recently open files (,f)"
+echo "ctrlp: Find file or a buffer(,j or c-f)" 
+echo "Nerdtree and openfile under cursor: Treemaps (,nn toggle and ,nb bookmark and ,nf find, gf go open file under cursor)"
+echo "Goyo.vim and vim-zenroom2: Removes all the distractions (,z)"
+echo ":w (,w)"
+echo "vim-yankstack: Maintains a history of previous yanks :yanks :registers (meta-p, meta-shift-p)"
+echo "vim-multiple-cursors: Select multiple cursors (c-n next and c-p previous and c-x skip)"
+echo "vim-fugitive: Git wrapper (:Gbrowse and :Gstatus and - for reset and p for patch and :Gcommit and :Gedit and :Gslipt and :Gvslipt and :Gtabedit and :Gdiff and :Gmove and :Ggrep and :Glog and :Gdelete and :Gread and :Gwrite)"
+echo "vim-expand-region: (+ to expand the visual selection and _ to shrink it)"
+echo "commentary-vim: Comments management (gcc for a line and gcap for a paragraph and gc in visual mode and :7,17Commentary)"
+echo "pathogen: Install plugins and manage your vim runtimepath (use 'installvimplugin' or 'git clone https://github.com/yourplugin ~/.vim_runtime/sources_non_forked/nameofplugin' for example"
+echo ""
+echo "Indenters"
+echo "vim-indent-object: Python indenter (ai and ii and al and il)"
+echo "Snippers"
+echo "snipmate: Alternative to ultisnips for snippers depending the filetype (forTAB for example)" 
+echo "Syntastics and linters"
+echo ":setlocal spell! (,ss)"
+echo "vim-syntastic : Common interface to syntax checkers for as many languages as possible (ACPI Source Language, ActionScript, Ada, Ansible configurations, API Blueprint, AppleScript, AsciiDoc, Assembly languages, BEMHTML, Bro, Bourne shell, C, C++, C#, Cabal, Chef, CMake, CoffeeScript, Coco, Coq, CSS, Cucumber, CUDA, D, Dart, DocBook, Dockerfile, Dust, Elixir, Erlang, eRuby, Fortran, Gentoo metadata, GLSL, Go, Haml, Haskell, Haxe, Handlebars, HSS, HTML, Java, JavaScript, JSON, JSX, Julia, LESS, Lex, Limbo, LISP, LLVM intermediate language, Lua, Markdown, MATLAB, Mercury, NASM, Nix, Objective-C, Objective-C++, OCaml, Perl, Perl 6, Perl POD, PHP, gettext Portable Object, OS X and iOS property lists, Pug (formerly Jade), Puppet, Python, QML, R, Racket, RDF TriG, RDF Turtle, Relax NG, reStructuredText, RPM spec, Ruby, SASS/SCSS, Scala, Slim, SML, Solidity, Sphinx, SQL, Stylus, Tcl, TeX, Texinfo, Twig, TypeScript, Vala, Verilog, VHDL, Vim help, VimL, Vue.js, xHtml, XML, XSLT, XQuery, YACC, YAML, YANG data models, YARA rules, z80, Zope page templates, and Zsh)"
+echo "merlin: Ocaml syntastic"
+echo "omnisharp: C# syntastic"
+#echo "vim-crystal: C but with Ruby syntax syntastic"
+#echo "vim-eastwood: Clojure linter syntastic"
+echo "rust: Rust syntastic (:RustFmt and :Rustplay)"
+echo "vim-swift: Swift syntastic (:help ft-swift)"
+echo "python-mode: Python syntastic (:help pymode)"
+echo "ghcmod-vim: Haskell syntastic (:help :filetype-overview and ghc-mod type and ghc-mod check or ghc-mod lint and ghc-mod expand and ghc-mod split)"
+echo ""
+echo "Completions"
+echo "neco-ghc: Haskell ghc-mod completion for neocomplcache/neocomplete/deoplete (:help compl-omni)"
+}
+echo vimfunctions >> $PATHOGENFOLDER/README
+
+
+### Tmux ###
 sudo pacman -S tmux  --noconfirm --needed
 sudo rm ~/.tmux.conf~
 cp ~/.tmux.conf ~/.tmux.conf~
 wget https://raw.githubusercontent.com/abueesp/Scriptnstall/master/.tmux.conf
 
-#Office
-wget https://raw.githubusercontent.com/abueesp/Scriptnstall/master/.bc
-sudo pacman -S libreoffice grc unoconv detox pandoc duplicity deja-dup --noconfirm --needed
-sudo pacman -S xmlstarlet jq datamash bc gawk mawk --noconfirm --needed #xml and jquery #wc join paste cut sort uniq
+
+### Office tools ###
+wget https://raw.githubusercontent.com/abueesp/Scriptnstall/master/.bc #My programmable calc
+sudo pacman -S libreoffice grc unoconv detox pandoc duplicity deja-dup --noconfirm --needed #Text tools
+sudo pacman -S xmlstarlet jq datamash bc gawk mawk --noconfirm --needed #XML and jquery #wc join paste cut sort uniq
 sudo pacman -S blender --noconfirm --needed
 
-#Other Tools
+
+### Other Tools ###
 sudo pacman -S brasero qemu archiso --noconfirm --needed
 sudo pacman -S traceroute nmap arp-scan --noconfirm --needed
 sudo pacman -S terminator htop autojump iotop vnstat at nemo task tree recordmydesktop --noconfirm --needed
 
-#Explorers
+
+### Web explorers ###
