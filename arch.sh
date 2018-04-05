bd9b233a339b1981e9cff9d308d17438e1f75fbe
abueesp <abueesp@gmail.com>
2018-04-05 06:53:06 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -320,7 +320,7 @@ pcm.!default {
 }" | sudo tee -a /etc/asound.conf
 cd ..
 sudo rm -r alsaequal #configure with alsamixer -D equal and alsaequal-mgr save myequalization
-#for advanced sound tools (Jack, Midis, etc) check my ubuntumusic script and https://wiki.archlinux.org/index.php/JACK_Audio_Connection_Kit and https://wiki.archlinux.org/index.php/Professional_audio and https://wiki.archlinux.org/index.php/USB_MIDI_keyboards
+#for more advanced sound tools (Jack, Midis, etc) check my studio/multimedia script and https://wiki.archlinux.org/index.php/JACK_Audio_Connection_Kit and https://wiki.archlinux.org/index.php/Professional_audio and https://wiki.archlinux.org/index.php/USB_MIDI_keyboards and https://github.com/nodiscc/awesome-linuxaudio
 
 #Search
 sudo pacman -S mlocate recoll --noconfirm --needed
