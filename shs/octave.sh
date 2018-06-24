a8cd9f970e249f811c665f1b5b282c98c84eaf9c
abueesp <abueesp@gmail.com>
2018-06-24 19:57:43 +0100

    Create octave.sh

---
+++ shs/octave.sh
@@ -0,0 +1,124 @@
+yaourt -S octave --noconfirm --needed
+#    A high-level language, primarily intended for numerical computations.
+yaourt -S cauchy --noconfirm --needed
+#    A library for transforming Matlab/Octave files to C++ (with Eigen) and Matlab
+yaourt -S circstat --noconfirm --needed
+#    The circular statistics toolbox for Matlab/Octave
+yaourt -S jsonlab --noconfirm --needed
+#    A toolbox to encode/decode JSON files in Matlab/Octave
+yaourt -S octave-audio --noconfirm --needed
+#    Audio recording, processing and playing tools.
+yaourt -S octave-cgi --noconfirm --needed
+#    Common Gatway Interface for Octave
+yaourt -S octave-communications --noconfirm --needed
+#    Digital Communications, Error Correcting Codes (Channel Code), Source Code
+yaourt -S octave --noconfirm --needed
+#    Computer-Aided Control System Design (CACSD) Tools for GNU Octave, based on
+yaourt -S octave-data-smoothing --noconfirm --needed
+#    Algorithms for smoothing noisy data
+yaourt -S octave-database --noconfirm --needed
+#    Interface to SQL databases, currently only postgresql using libpq.
+yaourt -S octave-dataframe --noconfirm --needed
+#    Data manipulation toolbox similar to R data.frame
+yaourt -S octave --noconfirm --needed
+#    divand performs an n-dimensional variational analysis (interpolation) of
+yaourt -S octave-econometrics --noconfirm --needed
+#    Econometrics functions including MLE and GMM based techniques.
+yaourt -S octave-financial --noconfirm --needed
+#    Financial manipulation, plotting functions and additional date manipulation
+yaourt -S octave-fl --noconfirm --needed
+#    The package contains code for basic functions in Fuzzy Logic for Octave.
+yaourt -S octave-fpl --noconfirm --needed
+#    Collection of routines to export data produced by Finite Elements or Finite
+yaourt -S octave-fuzzy-logic-toolkit --noconfirm --needed
+#    A mostly MATLAB-compatible fuzzy logic toolkit for Octave.
+yaourt -S octave-ga --noconfirm --needed
+#    Genetic optimization code.
+yaourt -S octave-general --noconfirm --needed
+#    General tools for Octave.
+yaourt -S octave-generate_html --noconfirm --needed
+#    Functions for generating HTML pages that contain help about functions.
+yaourt -S octave-geometry --noconfirm --needed
+#    Library for geometric computing extending MatGeom functions.
+yaourt -S octave-gsl --noconfirm --needed
+#    Octave bindings to the GNU Scientific Library.
+yaourt -S octave-hg --noconfirm --needed
+#    A high-level language, primarily intended for numerical computations.
+yaourt -S octave-image --noconfirm --needed
+#    The Octave-forge Image Aquisition package provides functions to capture.
+yaourt -S octave-informationtheory --noconfirm --needed
+#    Functions and routines for basic Information Theory definitions, and source.
+yaourt -S octave-instrument-control --noconfirm --needed
+#    Low level I/O functions for serial, i2c, parallel, tcp, gpib, vxi11 usbtmc.
+yaourt -S octave-io --noconfirm --needed
+#    Input/Output in external formats.
+yaourt -S octave-linear-algebra --noconfirm --needed
+#    Additional linear algebra code, including general SVD and matrix functions.
+yaourt -S octave-mapping --noconfirm --needed
+#    Simple Mapping and GIS .shp file functions.
+yaourt -S octave-mechanics --noconfirm --needed
+#    Library with functions useful for numerical computation in classical mechanics.
+yaourt -S octave-miscellaneous --noconfirm --needed
+#    Miscellaneous tools that don't fit somewhere else.
+yaourt -S octave-msh --noconfirm --needed
+#    Create and manage triangular and tetrahedral meshes for Finite Element or FVPDE solvers.
+yaourt -S octave-multicore --noconfirm --needed
+#    An Octave-forge package providing functions for parallel processing.
+yaourt -S octave-mvn --noconfirm --needed
+#    Multivariate normal distribution clustering and utility functions.
+yaourt -S octave-nan --noconfirm --needed
+#    A statistics and machine learning toolbox for data with and w/o missing values.
+yaourt -S octave-ncarray --noconfirm --needed
+#    Access a single or a collection of NetCDF files as a multi-dimensional array
+yaourt -S octave-netcdf --noconfirm --needed
+#    A MATLAB compatible NetCDF interface for Octave
+yaourt -S octave-nnet --noconfirm --needed
+#    The Octave-forge Nnet package for working with Neural Networks.
+yaourt -S octave-ocs --noconfirm --needed
+#    Package for solving DC and transient electrical circuit equations
+yaourt -S octave-odepkg --noconfirm --needed
+#    A package for solving ordinary differential equations and more.
+yaourt -S octave-optics --noconfirm --needed
+#    Functions covering various aspects of optics
+yaourt -S octave-optim --noconfirm --needed
+#    Non-linear optimization toolkit.
+yaourt -S octave-optiminterp --noconfirm --needed
+#    An optimal interpolation toolbox for octave and n-dimensions.
+yaourt -S octave-parallel --noconfirm --needed
+#    Parallel execution package. See also package mpi, maintained by Carlo.
+yaourt -S octave-plot --noconfirm --needed
+#    Additional ploting tools for Octave.
+yaourt -S octave-quaternion --noconfirm --needed
+#    Quaternion package for GNU Octave, includes a quaternion class.
+yaourt -S octave-queueing --noconfirm --needed
+#    The queueing package provides functions for queueing networks and Markov.
+yaourt -S octave-secs1d --noconfirm --needed
+#    A Drift-Diffusion simulator for 1d semiconductor devices
+yaourt -S octave-secs2d --noconfirm --needed
+#    A Drift-Diffusion simulator for 2d semiconductor devices
+yaourt -S octave-secs3d --noconfirm --needed
+#    A Drift-Diffusion simulator for 3d semiconductor devices
+yaourt -S octave-signal --noconfirm --needed
+#    Signal processing tools, including filtering, windowing and display functions.
+yaourt -S octave-sockets --noconfirm --needed
+#    Socket functions for networking from within octave.
+yaourt -S octave-specfun --noconfirm --needed
+#    Special functions including ellipitic functions, etc
+yaourt -S octave-statistics --noconfirm --needed
+#    Additional statistics functions for Octave.
+yaourt -S octave-strings --noconfirm --needed
+#    Additional functions for manipulation and analysis of strings.
+yaourt -S octave-struct --noconfirm --needed
+#    Additional Structure manipulations functions.
+yaourt -S octave-symbolic --noconfirm --needed
+#    Adds symbolic calculation features to GNU Octave. These include Algebra, Calculus, Fourier, Laplace...
+yaourt -S octave-tsa --noconfirm --needed
+#    Stochastic concepts and maximum entropy methods for time series analysis
+yaourt -S octave-video --noconfirm --needed
+#    A wrapper for ffmpeg libavformat and libavcodec, implementing addframe, avifile, aviinfo and aviread.
+yaourt -S python-oct2py --noconfirm --needed
+#    Python to GNU Octave bridge.
+yaourt -S python2-oct2py --noconfirm --needed
+#    Python to GNU Octave bridge.
+yaourt -S vim-octave --noconfirm --needed
+#    Vim file syntax highlighting
