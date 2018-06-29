58388cc9f660df7bb9193cf75cdbe662118aa2ce
abueesp <abueesp@gmail.com>
2018-06-29 05:37:40 +0100

    Create sagemath.sh

---
+++ shs/sagemath.sh
@@ -0,0 +1,10 @@
+yaourt -S sagemath --noconfirm 
+echo "Open Source Mathematics Software"
+yaourt -S sage-data-elliptic_curves --noconfirm 
+echo "Database of elliptic curves for SageMath"
+yaourt -S sage-notebook 1 --noconfirm.
+echo "Browser-based notebook interface for SageMath"
+yaourt -S sagemath-jupyter --noconfirm 
+echo "Jupyter kernel for SageMath"
+yaourt -S sagetex --noconfirm 
+echo "LaTeX documents"
